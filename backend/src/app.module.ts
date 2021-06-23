db2e4971f81cd78bde6c366745ccc1a48c78a442
kimyounghoon <choosey_@.naver.com>
2021-06-23 19:46:38 +0900

    [add] jwt module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,11 @@ import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import * as Joi from "joi";
 import { join } from "path";
-import { RestaurantsModule } from "./restaurants/restaurants.module";
-import { Restaurant } from "./restaurants/entities/restauarnts.entity";
+
 import { UsersModule } from "./users/users.module";
 import { CommonModule } from "./common/common.module";
 import { User } from "./users/entities/user.entity";
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
   imports: [
@@ -42,6 +42,7 @@ import { User } from "./users/entities/user.entity";
     }),
     UsersModule,
     CommonModule,
+    JwtModule,
   ],
   controllers: [],
   providers: [],
