300d9c8e06b4ac019ac5ae56361d7f2bafd33fca
younghoonKimm <choosey_@naver.com>
2021-12-12 16:21:32 +0900

    [add] react-hook-form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
-import { useQuery, useReactiveVar } from "@apollo/client";
 import React from "react";
+import { useReactiveVar } from "@apollo/client";
+
+import { isLoggedInVar } from "./apollo";
+import LoggedInRouter from "./routers/LoggedInRouter";
 import LoggedOutRouter from "./routers/LoggedOutRouter";
 
 // const IS_LOGGED_IN = gql`
@@ -8,11 +11,9 @@ import LoggedOutRouter from "./routers/LoggedOutRouter";
 //   }
 // `;
 
-import { isLoggedInVar } from "./apollo";
-
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-  return isLoggedIn && <LoggedOutRouter></LoggedOutRouter>;
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
