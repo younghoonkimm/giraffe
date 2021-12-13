1cb8767873cc8da700da3d8df1de63f97a49ab79
younghoonKimm <choosey_@naver.com>
2021-12-13 13:46:11 +0900

    [add] login page 스타일링

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -3,17 +3,14 @@ import React from "react";
 function Login() {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
         <h3 className="font-bold text-3xl text-gray-800">Log In</h3>
         <form className="flex flex-col mt-5 px-5">
-          <input
-            className="bg-gray-100 mb-3 py-3 px-2 rounded-lg shadow-inner hover:bg-purple-600"
-            placeholder="Email"
-          />
-          <input
-            className="bg-gray-100 mb-3 py-3 px-2 rounded-lg shadow-inner hover:bg-purple-600"
-            placeholder="Password"
-          />
+          <input className="input mb-3" placeholder="Email" />
+          <input className="input mb-3" placeholder="Password" />
+          <button className="btn" type="submit">
+            Login
+          </button>
         </form>
       </div>
     </div>
