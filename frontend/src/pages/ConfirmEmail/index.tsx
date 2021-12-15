70f260974d7f531eb98a73c7c90bbece53a98492
younghoonKimm <choosey_@naver.com>
2021-12-15 18:15:53 +0900

    [add] restaurants page

--- frontend/src/pages/ConfirmEmail/index.tsx
+++ frontend/src/pages/ConfirmEmail/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 import { gql, useApolloClient, useMutation } from "@apollo/client";
 import { verifyEmail, verifyEmailVariables } from "../../__generated__/verifyEmail";
 import { useMe } from "../../hooks/useMe";
@@ -59,6 +60,9 @@ const ConfirmEmail = () => {
 
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
+      <Helmet>
+        <title>Confirm Email | NuerEats</title>
+      </Helmet>
       <h2 className="text-lg mb-1 font-medium">Confirming email...</h2>
       <h4 className="text-gray-700 text-sm">Please wait, don't close this page</h4>
     </div>
--- frontend/src/pages/Restaurants/index.tsx
+++ frontend/src/pages/Restaurants/index.tsx
@@ -1,5 +1,48 @@
-function Restaurants() {
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { restaurantsPageQuery, restaurantsPageQueryVariables } from "../../__generated__/restaurantsPageQuery";
+
+const RESTAURANTS_QUERY = gql`
+  query restaurantsPageQuery($input: RestaurantsInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImg
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImg
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
+
+const Restaurants = () => {
+  const { data, loading, error } = useQuery<restaurantsPageQuery, restaurantsPageQueryVariables>(RESTAURANTS_QUERY, {
+    variables: {
+      input: {
+        page: 1,
+      },
+    },
+  });
+
   return <h1>Restaurants</h1>;
-}
+};
 
 export default Restaurants;
