6d0fdf530ecda9221a81b3414acdb4459b803a57
younghoonKimm <choosey_@naver.com>
2021-12-19 12:38:49 +0900

    [add] restaurants layout

--- frontend/src/pages/Restaurants/index.tsx
+++ frontend/src/pages/Restaurants/index.tsx
@@ -42,7 +42,25 @@ const Restaurants = () => {
     },
   });
 
-  return <h1>Restaurants</h1>;
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input className="input w-4/12 rounded-md border-0" type="search" placeholder="Search Restaurants"></input>
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {data?.allCategories.categories?.map((category) => (
+              <div className="flex flex-column items-center">
+                <div className="w-14 h-14 rounded-full bg-gray-100"></div>
+                <div key={category.id}>{category.name}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Restaurants;
