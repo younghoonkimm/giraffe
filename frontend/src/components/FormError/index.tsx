81da2fb61fc0d8fc18b410cad522b57e194aa1b9
younghoonKimm <choosey_@naver.com>
2021-12-23 10:09:20 +0900

    [add] error code role 추가

--- frontend/src/components/FormError/index.tsx
+++ frontend/src/components/FormError/index.tsx
@@ -2,5 +2,7 @@ import React from "react";
 import { FormErrorType } from "./type";
 
 export const FormError: React.FC<FormErrorType> = ({ errorMessage }) => (
-  <span className="text-medium text-red-500">{errorMessage}</span>
+  <span className="text-medium text-red-500" role="alert">
+    {errorMessage}
+  </span>
 );
