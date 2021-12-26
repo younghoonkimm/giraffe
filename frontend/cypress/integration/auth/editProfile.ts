5dd3dd5500ddd0faad8c0dcc125eb1b0afd66a14
younghoonKimm <choosey_@naver.com>
2021-12-26 10:37:24 +0900

    [add] edit profile testcode

---
+++ frontend/cypress/integration/auth/editProfile.ts
@@ -0,0 +1,17 @@
+describe("Edit Profile", () => {
+  const user = cy;
+  beforeEach(() => {
+    // @ts-ignore
+    user.login("kxkm09@nater.com", "adfsds!318");
+  });
+  it("can go to /edit-profile using the header", () => {
+    user.get('a[href="/edit-profile"]').click();
+    user.wait(2000);
+    user.title().should("eq", "Edit Profile | Giraffe");
+  });
+  it("can change email", () => {
+    user.visit("/edit-profile");
+    user.findByPlaceholderText(/email/i).clear().type("kxkm09@nates.com");
+    user.findByRole("button").click();
+  });
+});
