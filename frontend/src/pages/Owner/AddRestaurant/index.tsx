2554fcf0d0aa8fe9b314fd95569c0b9b5611c833
younghoonKimm <choosey_@naver.com>
2021-12-28 17:25:17 +0900

    [add] add mutation and form

--- frontend/src/pages/Owner/AddRestaurant/index.tsx
+++ frontend/src/pages/Owner/AddRestaurant/index.tsx
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { gql, useMutation } from "@apollo/client";
 import { createRestaurant } from "../../../__generated__/createRestaurant";
 import { createAccountMutationVariables } from "../../../__generated__/createAccountMutation";
+import { useForm } from "react-hook-form";
+import { AddRestaurantForm } from "./type";
+import { Button } from "../../../components/Button";
 
 const CREATE_RESTAURANT_MUTATION = gql`
   mutation createRestaurant {
@@ -17,5 +19,54 @@ export const AddRestaurant = () => {
   const [createRestaurantMutation, { loading, data }] = useMutation<createRestaurant, createAccountMutationVariables>(
     CREATE_RESTAURANT_MUTATION
   );
-  return <div className="container">add</div>;
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState,
+    formState: { errors },
+  } = useForm<AddRestaurantForm>({ mode: "onChange" });
+
+  const onSubmit = () => {};
+
+  return (
+    <div className="container">
+      <Helmet>
+        <title>Create Restaurant | Giraffe</title>
+      </Helmet>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="input"
+          {...register("name", {
+            required: "name is required",
+          })}
+          name="name"
+          type="text"
+          required
+          placeholder="name"
+        />
+        <input
+          className="input"
+          {...register("address", {
+            required: "name is required",
+          })}
+          name="address"
+          type="text"
+          required
+          placeholder="address"
+        />
+        <input
+          className="input"
+          {...register("categoryName", {
+            required: "name is required",
+          })}
+          name="categoryName"
+          type="text"
+          required
+          placeholder="categoryName"
+        />
+        <Button canClick={formState.isValid} loading={loading} actionText="Create Restaurant" buttonType="submit" />
+      </form>
+    </div>
+  );
 };
